91d6710b09b5ea9667a6a44ca377061fb7507ee0
Nicole Franca <nickmarquesf2000@gmail.com>
2021-04-15 13:03:24 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+using MySql.Data.MySqlClient;
+
+namespace Aula8
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection con = new MySqlConnection("server=localhost;database=aula8;uid=root;pwd=senha");
+        MySqlCommand cmd;
+        MySqlDataAdapter adapt;
+        int ID = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExibirDados();
+        }
+
+        private void ExibirDados()
+        {
+            try
+            {
+                con.Open();
+                DataTable dt = new DataTable();
+                adapt = new MySqlDataAdapter("SELECT * FROM Contatos", con);
+                adapt.Fill(dt);
+                dgvAgenda.DataSource = dt;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                con.Close();
+            }
+        }
+
+        private void LimparDados()
+        {
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCelular.Text = "";
+            txtTelefone.Text = "";
+            txtEmail.Text = "";
+            ID = 0;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja Sair do programa ?", "Agenda", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            else
+            {
+                txtNome.Focus();
+            }
+        }
+
+        private void btn_about_Click(object sender, EventArgs e)
+        {
+            txtNome.Focus();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //tudo tem que estar preenchido txtNome.Text !=""(diferente de vazio) && (concatena)
+            if (txtNome.Text != "" && txtEndereco.Text != "" && txtCelular.Text != "" && txtTelefone.Text != "" && txtEmail.Text != "")
+            {
+                try
+                {
+                    cmd = new MySqlCommand("INSERT INTO Contatos(nome,endereco,celular,telefone,email) VALUES(@nome,@endereco,@celular,@telefone,@email)", con);
+                    //abre a conexão
+                    con.Open();
+                    //substitui o values 
+                    //Toupper() converte para maisculo oque estiver entrando 
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@celular", txtCelular.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text.ToLower());
+                    //executa o comando
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Registro incluído com sucesso...");
+                }
+                catch (Exception ex)
+                {
+                    //se der algum erro
+                    MessageBox.Show("Erro : " + ex.Message);
+                }
+                finally
+                {
+                    //fecha conexão
+                    con.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text != "" && txtEndereco.Text != "" && txtCelular.Text != "" && txtTelefone.Text != "" && txtEmail.Text != "")
+            {
+                try
+                {
+                    //nome=@nome pq esta atualizando os registros
+                    cmd = new MySqlCommand("UPDATE Contatos SET nome=@nome, endereco=@endereco, celular=@celular,telefone=@telefone,email=@email WHERE id=@id", con);
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@id", ID);
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@endereco", txtEndereco.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@celular", txtCelular.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@telefone", txtTelefone.Text.ToUpper());
+                    cmd.Parameters.AddWithValue("@email", txtEmail.Text.ToLower());
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Registro atualizado com sucesso...");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro : " + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                    ExibirDados();
+                    LimparDados();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Informe todos os dados requeridos");
+            }
+        }
+
+        private void dgvAgenda_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                ID = Convert.ToInt32(dgvAgenda.Rows[e.RowIndex].Cells[0].Value.ToString());
+                txtNome.Text = dgvAgenda.Rows[e.RowIndex].Cells[1].Value.ToString();
+                txtEndereco.Text = dgvAgenda.Rows[e.RowIndex].Cells[2].Value.ToString();
+                txtCelular.Text = dgvAgenda.Rows[e.RowIndex].Cells[3].Value.ToString();
+                txtTelefone.Text = dgvAgenda.Rows[e.RowIndex].Cells[4].Value.ToString();
+                txtEmail.Text = dgvAgenda.Rows[e.RowIndex].Cells[5].Value.ToString();
+            }
+            catch { }
+        }
+
+        //So limpando os campos e voltando com o cursos pro inicio com o txt.Nome.Focus();
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            txtNome.Text = "";
+            txtEndereco.Text = "";
+            txtCelular.Text = "";
+            txtTelefone.Text = "";
+            txtEmail.Text = "";
+            txtNome.Focus();
+        }
+
+        private void btnDeletar_Click(object sender, EventArgs e)
+        {
+            if (ID != 0)
+            {
+                if (MessageBox.Show("Deseja Deletar este registro ?", "Agenda", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+                        cmd = new MySqlCommand("DELETE from Contatos WHERE id=@id", con);
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@id", ID);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("registro deletado com sucesso...!");
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro : " + ex.Message);
+                    }
+                    finally
+                    {
+                        con.Close();
+                        ExibirDados();
+                        LimparDados();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione um registro para deletar");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
